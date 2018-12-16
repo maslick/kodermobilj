6d32a35011dbb4c6aaea0db1ab163f5a7dc145bc
Pavel Maslov <pavel.masloff@gmail.com>
2018-12-16 23:43:11 +0100

    removed oauth

--- app/src/main/java/io/maslick/kodermobile/Config.kt
+++ app/src/main/java/io/maslick/kodermobile/Config.kt
@@ -1,10 +1,6 @@
 package io.maslick.kodermobile
 
 object Config {
-    const val clientId = "[CLIENT_ID]"
-    const val baseUrl = "[KEYCLOAK_URL]/auth/realms/[REALM]/protocol/openid-connect"
-    const val authenticationCodeUrl = "$baseUrl/auth"
-    const val redirectUri = "https://maslick.io/barkoder"
-    const val barkoderBaseDevUrl = ""
-    const val barkoderBaseProdUrl = ""
+    const val barkoderBaseDevUrl = "https://barkoder-dev.herokuapp.com"
+    const val barkoderBaseProdUrl = "https://barkoder.herokuapp.com"
 }
