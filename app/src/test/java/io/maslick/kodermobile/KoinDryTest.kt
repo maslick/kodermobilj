ab46732fdd297362d9ca0a8239e5cd8f09b88600
Pavel Maslov <pavel.masloff@gmail.com>
2018-12-21 18:35:50 +0100

    fixed tests (after refactoring)

--- app/src/test/java/io/maslick/kodermobile/KoinDryTest.kt
+++ app/src/test/java/io/maslick/kodermobile/KoinDryTest.kt
@@ -1,7 +1,7 @@
 package io.maslick.kodermobile
 
 import io.maslick.kodermobile.di.barkoderApi
-import io.maslick.kodermobile.di.keycloakApi
+import io.maslick.kodermobile.di.cache
 import io.maslick.kodermobile.di.mvp
 import org.junit.Test
 import org.koin.test.KoinTest
@@ -11,6 +11,6 @@ class KoinDryTest : KoinTest {
 
     @Test
     fun itemsModuleDryRunTest() {
-        checkModules(listOf(mvp, barkoderApi, keycloakApi))
+        checkModules(listOf(mvp, barkoderApi, cache))
     }
 }
