6a47aa94f59a4e1d913aabcf72f42b75f5a0a783
Pavel Maslov <pavel.maslov@ineor.si>
2019-04-30 11:16:30 +0200

    changed kc url

--- app/src/main/java/io/maslick/kodermobile/Config.kt
+++ app/src/main/java/io/maslick/kodermobile/Config.kt
@@ -1,9 +1,12 @@
 package io.maslick.kodermobile
 
 object Config {
-    const val barkoderBaseUrl = "https://barkoder.herokuapp.com"
-    const val keycloakBaseUrl = "https://activeclouder.ijs.si/auth/realms/barkoder/protocol/openid-connect"
+    const val keycloakUrl = "https://keycloak-maslick-io.1d35.starter-us-east-1.openshiftapps.com"
+    const val realm = "barkoder"
     const val clientId = "barkoder-frontend"
+    const val keycloakBaseUrl = "$keycloakUrl/auth/realms/$realm/protocol/openid-connect"
     const val authenticationCodeUrl = "$keycloakBaseUrl/auth"
+
+    const val barkoderBaseUrl = "https://barkoder.herokuapp.com"
     const val redirectUri = "barkoder://oauthresponse"
 }
