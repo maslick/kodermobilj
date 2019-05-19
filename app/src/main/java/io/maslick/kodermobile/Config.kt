a628c446b8cc37c19535b500b48503cbdcaebf61
Pavel Maslov <pavel.maslov@ineor.si>
2019-05-19 22:43:32 +0200

    * kotlin plugin version bump * gradle plugin version bump * updated kc_url and api_url

--- app/src/main/java/io/maslick/kodermobile/Config.kt
+++ app/src/main/java/io/maslick/kodermobile/Config.kt
@@ -1,12 +1,12 @@
 package io.maslick.kodermobile
 
 object Config {
-    const val keycloakUrl = "https://keycloak-maslick-io.1d35.starter-us-east-1.openshiftapps.com"
+    const val keycloakUrl = "https://auth.maslick.ru"
     const val realm = "barkoder"
     const val clientId = "barkoder-frontend"
     const val keycloakBaseUrl = "$keycloakUrl/auth/realms/$realm/protocol/openid-connect"
     const val authenticationCodeUrl = "$keycloakBaseUrl/auth"
 
-    const val barkoderBaseUrl = "https://barkoder.herokuapp.com"
+    const val barkoderBaseUrl = "https://koder-api.maslick.ru"
     const val redirectUri = "barkoder://oauthresponse"
 }
